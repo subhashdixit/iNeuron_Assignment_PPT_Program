dc0e0f32916a83be6d5f7b720e06c859a935be06
Subhash Dixit <subhashdixit17@gmail.com>
2023-06-18 13:04:30 +0530

    Update DSA _Mock_Test_2 _Python.py

--- DSA _Mock_Test_2 _Python.py
+++ DSA _Mock_Test_2 _Python.py
@@ -1,3 +1,15 @@
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well. You must not use any built-in exponent function or operator. 
+
+#  Example 1:
+# Input: x = 4 Output: 2 Explanation: The square root of 4 is 2, so we return 2.
+# Example 2:
+
+# Input: x = 8 Output: 2 Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+# Constraints:
+
+# 0 <= x <= 2^31 - 1
+
+# Note: Create a GitHub file for the solution and add the file link the the answer section below.
 def mySqrt(x):
     if x == 0:
         return 0
@@ -18,3 +30,63 @@ def mySqrt(x):
 x = 8
 result = mySqrt(x)
 print(result)
+
+# This code will output 2 for the given example input x = 8.
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+# Example 1:
+
+# Input: l1 = [2,4,3], l2 = [5,6,4] Output: [7,0,8] Explanation: 342 + 465 = 807.
+
+# Example 2:
+
+# Input: l1 = [0], l2 = [0] Output: [0]
+
+# Example 3:
+
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] Output: [8,9,9,9,0,0,0,1]
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def addTwoNumbers(l1, l2):
+    dummy = ListNode(0)  # Create a dummy node to hold the result
+    current = dummy
+    carry = 0
+
+    while l1 or l2 or carry:
+        sum = carry
+
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+        if l2:
+            sum += l2.val
+            l2 = l2.next
+
+        carry = sum // 10
+        current.next = ListNode(sum % 10)
+        current = current.next
+
+    return dummy.next
+
+# Example usage:
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = addTwoNumbers(l1, l2)
+while result:
+    print(result.val, end=" ")
+    result = result.next
+
+#     This code will output 7 0 8 for the given example input l1 = [2,4,3] and l2 = [5,6,4].
